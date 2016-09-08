5d4e705ee1346f63dc9e34716c8d5b51f81ac761
correyl <correyl@sfu.ca>
2016-09-07 17:36:17 -0700

    Making progress on Question 2

--- hello.go
+++ hello.go
@@ -1,5 +1,8 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func isPrime(x int) bool{
 	for a:=x-1; a>1; a--{
@@ -23,13 +26,42 @@ func countPrimes(x int) int{
 	return total
 }
 
-func countStrings() map[string]int{
-	
+func countStrings(filename string) map[string]int{
+	var m map[string]int
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b)
+	var current string
+	current = ""
+	for a:=0;a<len(str);a++{
+		if(string(str[a]) != string(" ") || string(str[a]) != string("\n")){
+			current = current + string(str[a])
+			fmt.Println(current)
+		} else {
+			fmt.Println(current)
+			if(m[current] == 0){
+				m[current] = 1
+			} else{
+				m[current] = m[current] + 1
+			}
+			current = ""
+		}
+	}
+	return m
 }
 
 func main() {
+	/*
 	fmt.Println("Input a number for countPrimes()")
 	var user_input int 
 	fmt.Scan(&user_input)
-  fmt.Println(countPrimes(user_input))
+  fmt.Println(countPrimes(user_input)) 
+	*/
+	fmt.Printf("{")
+	for x, y := range countStrings("textfile.txt") { 
+		fmt.Printf(x, ":",y)
+	}
+	fmt.Printf("}")
 }
