e8edf89997aca208b24482ea84f59fbf4ae4fe7c
correyl <correyl@sfu.ca>
2016-09-08 17:25:27 -0700

    Making progress on tests for Time24

--- GoProblemSet/ps1_test.go
+++ GoProblemSet/ps1_test.go
@@ -58,6 +58,47 @@ func TestCountstrings(t *testing.T){
 
 // Question 3
 func TestTime24(t *testing.T){
+	var times = []Time24{
+		{12, 30, 45},
+		{12, 30, 45},
+		{25, 61, 61},
+		{1,1,1},
+	}
+	
+	// Testing validTime24()
+	if( !validTime24(times[0]) ) { // Valid time passed in
+		t.Error(
+			"For ", times[0],
+			"Got False",
+			"Expected True",
+		)
+	}
+	
+	if( validTime24(times[2]) ){ // Invalid time passed in
+		t.Error(
+			"For ", times[2],
+			"Got True",
+			"Expected False",
+		)
+	}
+	
+	// Testing equalsTime24()
+	if( !equalsTime24(times[0], times[1]) ){ // Equal times passed in
+		t.Error(
+			"For ", times[0], "and ", times[1],
+			"Expected True",
+			"Got False",
+		)
+	}
+	if( equalsTime24(times[0], times[3]) ){ // Unequal times passed in
+		t.Error(
+			"For ", times[0], "and ", times[3],
+			"Expected True",
+			"Got False",
+		)
+	}
+	
+	// Testing lessThanTime24()
 	
 }
 
