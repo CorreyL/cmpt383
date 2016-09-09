6b9fbdafbe335b9d6dd6decabab5900b3500f76f
correyl <correyl@sfu.ca>
2016-09-08 17:15:21 -0700

    Test created for countStrings()

--- GoProblemSet/ps1_test.go
+++ GoProblemSet/ps1_test.go
@@ -1,6 +1,9 @@
 package ps1
 
-import "testing"
+import(
+ "testing"
+ "strconv"
+)
 
 // Question 1
 func TestCountprimes(t *testing.T){
@@ -26,3 +29,35 @@ func TestCountprimes(t *testing.T){
 }
 
 // Question 2
+func TestCountstrings(t *testing.T){
+	var final string
+	final = ""
+	final = final + "{"
+	len_check := 0 // Ensures that the string ", " is not printed for the final pair
+	m := countStrings("textfile.txt")
+	for x, y := range m { 
+		final = final + "\"" + x + "\"" + ":" + strconv.Itoa(y)
+		if(len_check != len(m)-1){
+			final = final + ", "
+		}
+		len_check++
+	}
+	final = final + "}"
+	
+	var expected string
+	expected = "{\"The\":1, \"big\":3, \"dog\":1, \"ate\":1, \"the\":1, \"apple\":1}"
+	
+	if(final != expected){
+		t.Error(
+			"For textfile.txt",
+			"Got ", final,
+			"Expected ", expected,
+		)
+	}
+}
+
+// Question 3
+func TestTime24(t *testing.T){
+	
+}
+
