c5b515a120444cab489a5a31c9e6e072464a9aff
Correy Lim <correyl@sfu.ca>
2016-09-21 14:50:40 -0700

    Program now prints a file's SHA-256 hash

--- GoProject/proj1.go
+++ GoProject/proj1.go
@@ -2,7 +2,31 @@
 package main
 
 import (
-	"fmt"
-	"crpyto/sha256"
+	// "fmt"
+	"crypto/sha256"
+	"os"
+	"io/ioutil"
+	"encoding/hex"
 )
+
+// os.Args[0] is the name of the program
+// os.Args[1] is the first argument passed into the program
+	// os.Args[1] will be the directory of that the program will work in
+
+func errorCheck(e error){
+	if(e != nil){
+		panic(e)
+	}
+}
+
+func main(){
+	hasher := sha256.New()
+	s, err := ioutil.ReadFile(os.Args[1])
+	hasher.Write(s)
+	errorCheck(err);
+	 os.Stdout.WriteString(hex.EncodeToString(hasher.Sum(nil)))
+	// fmt.Println(os.Args[1])
+}
+
+
 
