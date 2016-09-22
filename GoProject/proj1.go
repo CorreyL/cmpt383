4264e7c0b95fd6c9bc7220250cff61477473b5eb
correyl <correyl@sfu.ca>
2016-09-22 15:44:03 -0700

    Adding comments for clarification

--- GoProject/proj1.go
+++ GoProject/proj1.go
@@ -34,7 +34,7 @@ newPath := strings.TrimSpace(split[len(split)-1])
 fmt.Println("The newPath is: ", newPath)
 */
 
-func visit(path string, h os.FileInfo, err error) error {
+func visit(path string, fi os.FileInfo, err error) error {
 	r, _ := regexp.Compile(`\.[a-zA-Z0-9]+`) // Regular Expression for the string of a file
 	if(r.MatchString(path)){
 		hasher := sha256.New()
@@ -51,19 +51,21 @@ func visit(path string, h os.FileInfo, err error) error {
 }
 
 func main(){
-	if( len(os.Args) < 2 ){
+	if( len(os.Args) < 2 ){ // If a directory was provided, then len(os.Args) >= 2 
 		fmt.Println("ERROR: A directory was not supplied to proj1.go.")
 		fmt.Println("Aborting program.")
 		os.Exit(1)
 	}
 	err := filepath.Walk(os.Args[1], visit)
 	errorCheck(err)
-	if( len(currentDirFiles) == 0 ){
-		fmt.Println("The directory provided either does not exist OR contains no files")
+	if( len(currentDirFiles) == 0 ){ // Checks to see if anything was added to the map
+		fmt.Println("The directory provided either does not exist OR contains no files.")
 		os.Exit(0)
 	}
+	noDups := true;
 	for x, y := range currentDirFiles {
 		if( strings.IndexAny(y,"|") != -1 ){
+			noDups = false;
 			split := strings.Split(y, "|")
 			fmt.Println("SHA-256 KEY:", x)
 			fmt.Println("Number of files with this signature:", len(split) )
@@ -72,6 +74,9 @@ func main(){
 			}
 			fmt.Printf("\n\n")
 		}
+	}
+	if( noDups ){
+		fmt.Println("No duplicate files were found!")
 	}
 }
 
